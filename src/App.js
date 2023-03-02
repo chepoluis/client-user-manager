332d642b3b98c868d66c2cdf538fe8308af1ab37
Luis <vsjoseluisvs@gmail.com>
2023-03-02 10:46:03 -0600

    themes added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
+import { ColorModeContext, useMode } from './theme';
+import { CssBaseline, ThemeProvider } from '@mui/material';
 
 function App() {
+  const [theme, colorMode] = useMode();
+
   return (
-    <div className="app">
-      :D
-    </div>
+    <ColorModeContext.Provider value={colorMode}>
+      <ThemeProvider theme={theme}>
+        {/* Resets the css to defaults. Default MUI styles are applied */}
+        <CssBaseline />
+
+        <div className="app">
+          <main className="content"></main>
+        </div>
+      </ThemeProvider>
+    </ColorModeContext.Provider>
   );
 }
 
