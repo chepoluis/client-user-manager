f63d98dc0992b9886c53613b7f39b876299a468e
José Luis <34282596+chepoluis@users.noreply.github.com>
2023-03-06 12:42:49 -0600

    login (#5)

--- src/scenes/login/index.jsx
+++ src/scenes/login/index.jsx
@@ -1,9 +1,113 @@
-import React from 'react'
+import { Box, Button, TextField, useTheme } from "@mui/material";
+import { Formik } from "formik";
+import * as yup from "yup";
+import useMediaQuery from "@mui/material/useMediaQuery";
+import Header from "../../components/Header";
+import { tokens } from "../../theme";
 
-const Login = () => {
+const Login = (props) => {
+  const theme = useTheme();
+  const colors = tokens(theme.palette.mode);
+
+  const isNonMobile = useMediaQuery("(min-width:700px)");
+
+  const handleFormSubmit = (values) => {
+    console.log(values);
+  };
+
   return (
-    <div>Login</div>
-  )
-}
+    <Box
+      m="20px"
+      sx={{
+        background: `${colors.primary[400]}`,
+        padding: "20px",
+        margin: "0",
+        borderRadius: "10px",
+        display: "flex",
+        flexDirection: "column",
+        position: "absolute",
+        top: "50%",
+        left: "50%",
+        transform: "translate(-50%, -50%)",
+        width: isNonMobile ? "450px" : "350px",
+      }}
+    >
+      <Header title="LOGIN" subtitle="Enter credentials to login" />
+
+      <Formik
+        onSubmit={handleFormSubmit}
+        initialValues={initialValues}
+        validationSchema={checkoutSchema}
+      >
+        {({
+          values,
+          errors,
+          touched,
+          handleBlur,
+          handleChange,
+          handleSubmit,
+        }) => (
+          <form onSubmit={handleSubmit}>
+            <Box
+              display="grid"
+              gap="30px"
+              gridTemplateColumns="repeat(2, minmax(0, 1fr))"
+              sx={{
+                "& > div": { gridColumn: isNonMobile ? undefined : "span 2" },
+              }}
+            >
+              <TextField
+                fullWidth
+                variant="filled"
+                type="text"
+                label="Email"
+                onBlur={handleBlur}
+                onChange={handleChange}
+                value={values.email}
+                name="email"
+                error={!!touched.email && !!errors.email}
+                helperText={touched.email && errors.email}
+                sx={{ gridColumn: "span 2" }}
+              />
+              <TextField
+                fullWidth
+                variant="filled"
+                type="password"
+                label="Password"
+                onBlur={handleBlur}
+                onChange={handleChange}
+                value={values.password}
+                name="password"
+                error={!!touched.password && !!errors.password}
+                helperText={touched.password && errors.password}
+                sx={{ gridColumn: "span 2" }}
+              />
+            </Box>
+            <Box mt="20px">
+              <Button
+                sx={{ width: "100%", fontSize: "1rem" }}
+                type="submit"
+                color="secondary"
+                variant="contained"
+              >
+                Login
+              </Button>
+            </Box>
+          </form>
+        )}
+      </Formik>
+    </Box>
+  );
+};
+
+const checkoutSchema = yup.object().shape({
+  email: yup.string().email("Invalid email").required("No email provided."),
+  password: yup.string().required("No password provided."),
+});
+
+const initialValues = {
+  email: "",
+  password: "",
+};
 
 export default Login;
